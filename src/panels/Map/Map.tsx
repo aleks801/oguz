e73809a802c3b753d19f9e0283851d0ca074cfc9
Aleksandr Denisevich <denisevich.aa@gmail.com>
2023-05-11 21:56:13 +0300

    feat: map items current progress

--- src/panels/Map/Map.tsx
+++ src/panels/Map/Map.tsx
@@ -3,8 +3,10 @@ import cn from "classnames"
 import styles from "./Map.module.css"
 import map from "../../assets/map.png"
 import pointRed from "../../assets/map_point_r.png"
+import pointViolent from "../../assets/map_point_v.png"
 import pointBlue from "../../assets/map_point_b.png"
 import lock from "../../assets/map_lock.png"
+import check from "../../assets/check.png"
 import bubble from "../../assets/map_bubble.png"
 import user from "../../assets/map_user.png"
 import { mapItems } from "./data"
@@ -16,12 +18,14 @@ type Props = {
   go: (panelId: string) => void
 }
 
+const currentProgress = 2
+
 export const Map = ({ id, go }: Props) => {
   const { setPopup } = useContext(PopupContext)
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      setPopup("intro")
+      // setPopup("intro")
     }, 2000)
 
     return () => clearTimeout(timeoutId)
@@ -32,28 +36,31 @@ export const Map = ({ id, go }: Props) => {
       <div className={styles.root}>
         <div className={styles.map}>
           <img src={map} />
-          {mapItems.map((item, i) => {
-            const isRed = item.type === "red"
+          {mapItems.map(({ id, text, style }, index) => {
+            const isCurrent = id === currentProgress
+            const isCompleted = currentProgress >= index + 1
 
             return (
               <div
-                className={cn(styles.point, { [styles.vertical]: isRed })}
-                key={i}
-                style={item.style}
-                onClick={() => (isRed ? go("lesson") : undefined)}
+                className={cn(styles.point, { [styles.vertical]: isCurrent })}
+                key={id}
+                style={style}
+                onClick={() => (isCurrent ? go("lesson") : undefined)}
               >
-                {isRed && (
+                {isCurrent && (
                   <div>
                     <img src={user} />
                   </div>
                 )}
                 <div className={styles.mark}>
-                  <div className={styles.label}>{isRed ? <span>{i + 1}</span> : <img src={lock} />}</div>
-                  <img src={isRed ? pointRed : pointBlue} />
+                  <div className={styles.label}>
+                    {isCurrent ? <span>{id}</span> : isCompleted ? <img src={check} /> : <img src={lock} />}
+                  </div>
+                  <img src={isCurrent ? pointRed : isCompleted ? pointViolent : pointBlue} />
                 </div>
                 <div className={styles.bubble}>
                   <img src={bubble} />
-                  <div className={styles.text}>{item.text}</div>
+                  <div className={styles.text}>{text}</div>
                 </div>
               </div>
             )
--- src/panels/Map/data.ts
+++ src/panels/Map/data.ts
@@ -1,6 +1,12 @@
-export const mapItems = [
-  { type: "red", text: "Вводный урок", style: { bottom: 45, left: "18%" } },
-  { type: "blue", text: "Главные ошибки", style: { bottom: 200, left: "38%" } },
-  { type: "blue", text: "Кредиты - это зло или возможность?", style: { bottom: 320, left: "32%" } },
-  { type: "blue", text: "Потери и мошенники", style: { bottom: 480, left: "45%" } },
+export type MapItem = {
+  text: string
+  style: { bottom: number | string; left: number | string }
+  id: number
+}
+
+export const mapItems: Array<MapItem> = [
+  { id: 1, text: "Вводный урок", style: { bottom: 45, left: "18%" } },
+  { id: 2, text: "Главные ошибки", style: { bottom: 200, left: "38%" } },
+  { id: 3, text: "Кредиты - это зло или возможность?", style: { bottom: 320, left: "32%" } },
+  { id: 4, text: "Потери и мошенники", style: { bottom: 480, left: "45%" } },
 ]
