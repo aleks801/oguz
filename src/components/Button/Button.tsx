af9a38c467b7ceb06911c8b0de12f07f8a150648
Aleksandr Denisevich <denisevich.aa@gmail.com>
2023-06-01 21:31:33 +0300

    feat: button disabled, yes no check

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,14 @@ import btnNext from "assets/btn_next.png"
 import btnYes from "assets/btn_yes.png"
 import btnYesGreen from "assets/btn_yes_green.png"
 import btnNo from "assets/btn_no.png"
+import styles from "./Button.module.css"
 
 type ButtonVariant = "green_next" | "next" | "yes" | "no" | "yes_green" | "green_next_big"
 
 type ButtonProps = {
   onClick?: () => void
   variant: ButtonVariant
+  disabled?: boolean
 }
 
 const variantImageMapping: Record<ButtonVariant, string> = {
@@ -21,6 +23,6 @@ const variantImageMapping: Record<ButtonVariant, string> = {
   green_next_big: btnNextGreenBig,
 }
 
-export const Button = ({ onClick, variant }: ButtonProps) => {
-  return <img src={variantImageMapping[variant]} onClick={onClick} />
+export const Button = ({ onClick, variant, disabled }: ButtonProps) => {
+  return <img className={disabled ? styles.disabled : ""} src={variantImageMapping[variant]} onClick={onClick} />
 }
--- src/components/YesNo/YesNo.tsx
+++ src/components/YesNo/YesNo.tsx
@@ -1,11 +1,18 @@
 import { Button } from "components/Button"
 import styles from "./YesNo.module.css"
+import { useState } from "react"
 
 export const YesNo = () => {
+  const [checked, setChecked] = useState<"yes" | "no" | undefined>()
+
+  const handleCheck = (variant: "yes" | "no") => {
+    setChecked(variant)
+  }
+
   return (
     <div className={styles.root}>
-      <Button variant="yes" />
-      <Button variant="no" />
+      <Button variant="yes" disabled={checked !== "yes"} onClick={() => handleCheck("yes")} />
+      <Button variant="no" disabled={checked !== "no"} onClick={() => handleCheck("no")} />
     </div>
   )
 }
