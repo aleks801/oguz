6d483e6235043d5111e8421cf39d0256b74cb065
Aleksandr Denisevich <denisevich.aa@gmail.com>
2023-06-05 22:01:02 +0300

    feat: fix speech baloons and attention wrappers

--- src/components/SpeechBaloon/SpeechBaloon.tsx
+++ src/components/SpeechBaloon/SpeechBaloon.tsx
@@ -2,14 +2,14 @@ import { Author } from "components/Author"
 import styles from "./SpeechBaloon.module.css"
 import back from "assets/speech_balloon_med.png"
 
-type Props = { children: React.ReactNode }
+type Props = { children: React.ReactNode; isSmall?: boolean }
 
-export const SpeechBaloon = ({ children }: Props) => {
+export const SpeechBaloon = ({ children, isSmall = false }: Props) => {
   return (
     <div>
       <div className={styles.root}>
         <img className={styles.back} src={back} />
-        <div className={styles.content}>
+        <div className={styles.content} style={{ paddingBottom: isSmall ? 18 : 40, paddingTop: isSmall ? 5 : 10 }}>
           <p>{children}</p>
         </div>
       </div>
